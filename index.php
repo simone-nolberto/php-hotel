22d107356f077ed080a71d2fb625136a722289a7
simone-nolberto <simone.nolberto@outlook.com>
2024-04-15 16:52:05 +0200

    update with table visualisation

--- index.php
+++ index.php
@@ -51,22 +51,39 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bool B/B</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <?php foreach ($hotels as $hotel) : ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrizione</th>
+                <th>Parcheggio</th>
+                <th>Voto</th>
+                <th>Distanza dal centro</th>
+            </tr>
+        </thead>
 
-        <ul>
-            <li><?= $hotel["name"] ?></li>
-            <li><?= $hotel["description"] ?></li>
-            <li><?= $hotel["parking"] ?></li>
-            <li><?= $hotel["vote"] ?></li>
-            <li><?= $hotel["distance_to_center"] ?> Km</li>
-        </ul>
+        <tbody>
 
+            <?php foreach ($hotels as $hotel) : ?>
 
-    <?php endforeach; ?>
+                <tr>
+                    <td><?= $hotel["name"] ?></td>
+                    <td><?= $hotel["description"] ?></td>
+                    <td><?= $hotel["parking"] ?></td>
+                    <td><?= $hotel["vote"] ?></td>
+                    <td><?= $hotel["distance_to_center"] ?> Km</td>
+                </tr>
+
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+
 
 </body>
 
